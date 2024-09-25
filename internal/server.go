6059359fb3403e268254dcdb7c6a970163d9a8dc
Adam Foryś <adam.forys@gmail.com>
2024-09-25 08:52:48 +0200

    Update server.go

--- internal/server.go
+++ internal/server.go
@@ -12,8 +12,8 @@ import (
 	"text/template"
 	"time"
 
-	"github.com/bernardo-bruning/ollama-copilot/internal/handlers"
-	"github.com/bernardo-bruning/ollama-copilot/internal/middleware"
+	"github.com/galachad/ollama-copilot/internal/handlers"
+	"github.com/galachad/ollama-copilot/internal/middleware"
 	"github.com/ollama/ollama/api"
 )
 
